cd28dd85de37e86527e2d0157f864f2c8faf1580
Isaac Hew <isaachewks@gmail.com>
2021-11-18 21:30:48 +0800

    Added tank upgrades Every tank "page" now displays what it upgrades to and what it upgrades from.

--- wiki.js
+++ wiki.js
@@ -1,26 +1,48 @@
 let mockups=[]
-fetch("https://womy-aras.herokuapp.com/mockups.json").then(a=>a.json()).then(a=>{
-    mockups=a
-
-    let roottank=mockups.findIndex(a=>a.name=="Basic")
-    allinpath(roottank,(a,b)=>{
-        paths[a]??=[]
-        paths[a].push(b)
-    })
-
-})
-tanks=new Set()
-paths={}
+let roottank
+let tanks=new Set()
+let tankpaths={}
 function allinpath(root,callback,prefix=[]){
+    callback(root,prefix)
     for(let i of mockups[root].upgrades){
-        let nprefix=[...prefix,i]
-        callback(i.index,nprefix)
-        allinpath(i.index,callback,nprefix)
+        allinpath(i.index,callback,[...prefix,root])
     }
 }
 
+
+fetch("https://womy-aras.herokuapp.com/mockups.json").then(a=>a.json()).then(a=>{
+    mockups=a
+
+    roottank=mockups.findIndex(a=>a.name=="Basic")
+    allinpath(roottank,(a,b)=>{
+        tankpaths[a]??={paths:[],upgradesfrom:new Set}
+        tankpaths[a].paths.push(b)
+        if(b.length)tankpaths[a].upgradesfrom.add(b[b.length-1])
+    })
+    load(roottank)
+})
+
 function load(id){
     document.getElementById("name").textContent=mockups[id].name
+    document.getElementById("upgradesto").textContent=""
+    for(var upg of mockups[id].upgrades){
+        let divw=document.createElement("div")
+        divw.textContent=mockups[upg.index].name
+        divw.className="tank"
+        divw.addEventListener("click",(ind=>a=>load(ind))(upg.index))
+
+        document.getElementById("upgradesto").append(divw)
+    }
+
+    document.getElementById("upgradesfrom").textContent=""
+    for(var branch of tankpaths[id].upgradesfrom){
+        let divw=document.createElement("div")
+        divw.textContent=mockups[branch].name
+        divw.className="tank"
+        divw.addEventListener("click",(ind=>a=>load(ind))(branch))
+
+        document.getElementById("upgradesfrom").append(divw)
+    }
 }
 
 document.getElementById("entityid").addEventListener("input",e=>{
